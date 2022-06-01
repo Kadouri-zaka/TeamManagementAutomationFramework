ec2918ba0fe1b7038529b3178be13472f7945541
Kadouri-zaka <79146327+Kadouri-zaka@users.noreply.github.com>
2022-06-01 12:23:43 +0100

    Update UploadFileForMonth.java

--- src/test/java/StepsDefinition/ScControl/UploadFileForMonth.java
+++ src/test/java/StepsDefinition/ScControl/UploadFileForMonth.java
@@ -10,7 +10,7 @@ import java.awt.*;
 public class UploadFileForMonth {
     CommonHelper commonHelper = new CommonHelper();
     DriverHandler driver= new DriverHandler();
-    String fimepath = new String("C:\\Users\\zkadouri\\Desktop\\newFunction.txt");
+    String fimepath = new String("C:\\Users\\zkad\\Desktop\\newFunction.txt");
     @And("User click on button upload")
     public void user_click_on_button_upload() throws InterruptedException, AWTException {
         commonHelper.waitElementToBeVisible(By.id("largeModelTMPTM"));
